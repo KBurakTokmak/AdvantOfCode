78d24553ff95bd73b59b943f389379b27f223c5d
Kadir Burak Tokmak <56766922+KBurakTokmak@users.noreply.github.com>
2025-12-04 11:26:51 +0300

    day4 finished

---
+++ AdventOfCode_2025/day4/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+std::vector<std::vector<char>> readGridFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    std::vector<std::vector<char>> grid;
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return grid;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::vector<char> row(line.begin(), line.end());
+        grid.push_back(row);
+    }
+
+    file.close();
+    return grid;
+}
+
+int main() {
+    std::string filename = "input.txt";
+    std::vector<std::vector<char>> table = readGridFromFile(filename);
+    long long access_count = 0;
+    int row_count = table.size();
+    int col_count = table[0].size();
+    for (int r = 0; r < row_count; ++r) {
+        for (int c = 0; c < col_count; ++c) {
+            if (table[r][c] != '@') continue;
+            int neigbour_count = 0;
+            for (int i = -1; i <= 1; ++i) {
+                for (int j = -1; j <= 1; ++j) {
+                    if(i == 0 && j == 0) continue;
+                    if(r + i < 0 || r + i >= row_count || c + j < 0 || c + j >= col_count) continue; // boundry or seg fault
+                    if (table[r + i][c + j] == '@') ++neigbour_count;
+                }
+            }
+            if (neigbour_count < 4) access_count++;
+        }
+    }
+    std::cout << "Access count: " << access_count << std::endl;
+
+
+
+    return 0;
+}
